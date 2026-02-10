fd5150fd1afc3fca81882b3f107a66750bcd18b0
Rodrigo Mello <rmello@geoforce.com>
2026-02-10 07:55:21 -0300

    add metadata to entries

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::fs;
 
 fn main() {
-    // read current directory
     let paths = fs::read_dir(".").unwrap();
 
     for path in paths {
         let entry = path.unwrap();
-        println!("{}", entry.file_name().to_string_lossy());
+        let metadata = entry.metadata().unwrap();
+        let name = entry.file_name();
+
+        if metadata.is_dir() {
+            println!("{}/", name.to_string_lossy());
+        } else {
+            println!("{}", name.to_string_lossy());
+        }
     }
+
 }
